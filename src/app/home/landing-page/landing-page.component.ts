2f6bf8877120237d3ff48d465db0c7c63af247ba
Pankaj Choudhary <pankajc@cloudthat.com>
2024-01-23 19:16:22 +0530

    First Commit

--- src/app/home/landing-page/landing-page.component.ts
+++ src/app/home/landing-page/landing-page.component.ts
@@ -12,77 +12,106 @@ import { LoginComponent } from '../login/login.component';
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
-  providers: [NgbCarouselConfig]
+  providers: [NgbCarouselConfig],
 })
-
 export class LandingPageComponent implements OnInit {
   title = 'ng-carousel-demo';
-  productList:IProduct[]=[]
+  productList: IProduct[] = [];
   private readonly notifier: NotifierService;
-  sessionFlag:boolean=false
+  sessionFlag: boolean = false;
 
-  membershipIcon='assets/premiumMembershp.jpg'
-loggedInUser:LoggedInUser=null
+  membershipIcon = 'assets/premiumMembershp.jpg';
+  loggedInUser: LoggedInUser = null;
   //This array is representing the upper carousel.
   images = [
-    {title: 'First Slide', short: 'First Slide Short', src: "assets/Product_Images/carousel1.jpg"},
-    {title: 'Second Slide', short: 'Second Slide Short', src: "assets/Product_Images/carousel2.jpg"},
-    {title: 'Third Slide', short: 'Third Slide Short', src: "assets/Product_Images/carousel3.jpg"}
+    {
+      title: 'First Slide',
+      short: 'First Slide Short',
+      src: 'assets/Product_Images/carousel1.jpg',
+    },
+    {
+      title: 'Second Slide',
+      short: 'Second Slide Short',
+      src: 'assets/Product_Images/carousel2.jpg',
+    },
+    {
+      title: 'Third Slide',
+      short: 'Third Slide Short',
+      src: 'assets/Product_Images/carousel3.jpg',
+    },
   ];
 
-
   //These are images of Products
-  productImages:string[]=[]
-//   ['assets/Product_Images/Point_and_shoot_cameras.jpg','assets/Product_Images/TV.jpg','assets/Product_Images/watch.jpg','assets/Product_Images/supplement.jpg',
-// 'assets/Product_Images/shirt.jpg','assets/Product_Images/eyewear.gif','assets/Product_Images/jeans.jpg','assets/Product_Images/jumpsuits.gifs'
+  productImages: string[] = [];
+  //   ['assets/Product_Images/Point_and_shoot_cameras.jpg','assets/Product_Images/TV.jpg','assets/Product_Images/watch.jpg','assets/Product_Images/supplement.jpg',
+  // 'assets/Product_Images/shirt.jpg','assets/Product_Images/eyewear.gif','assets/Product_Images/jeans.jpg','assets/Product_Images/jumpsuits.gifs'
 
-// ]
+  // ]
 
   //This array is representing the lower carousel
   images_Lower = [
-    {title: 'First Slide', short: 'First Slide Short', src: "assets/Product_Images/carousel5.jpg"},
-    {title: 'Second Slide', short: 'Second Slide Short', src: "assets/Product_Images/carousel5.jpg"},
-    {title: 'Third Slide', short: 'Third Slide Short', src: "assets/Product_Images/carousel6.jpg"}
+    {
+      title: 'First Slide',
+      short: 'First Slide Short',
+      src: 'assets/Product_Images/carousel5.jpg',
+    },
+    {
+      title: 'Second Slide',
+      short: 'Second Slide Short',
+      src: 'assets/Product_Images/carousel5.jpg',
+    },
+    {
+      title: 'Third Slide',
+      short: 'Third Slide Short',
+      src: 'assets/Product_Images/carousel6.jpg',
+    },
   ];
-  constructor(config: NgbCarouselConfig,private router: Router,private service: HomePageService,private active_route: ActivatedRoute, notifierService: NotifierService,public dialog: MatDialog, public loginDialog:MatDialog) {
+  constructor(
+    config: NgbCarouselConfig,
+    private router: Router,
+    private service: HomePageService,
+    private active_route: ActivatedRoute,
+    notifierService: NotifierService,
+    public dialog: MatDialog,
+    public loginDialog: MatDialog
+  ) {
     config.interval = 1500;
     config.keyboard = true;
     config.pauseOnHover = true;
     this.notifier = notifierService;
-    this.loggedInUser=sessionStorage.getItem('userEmailID')    
+    this.loggedInUser = sessionStorage.getItem('userEmailID');
   }
   ngOnInit(): void {
-    this.FetchProducts()
-    if(sessionStorage.getItem('userEmailID')!=null){
-      this.loggedInUser=sessionStorage.getItem('userEmailID')
+    this.FetchProducts();
+    if (sessionStorage.getItem('userEmailID') != null) {
+      this.loggedInUser = sessionStorage.getItem('userEmailID');
     }
-    console.log('reached constructor')
-    }
+    console.log('reached constructor');
+  }
 
-
-
-  FetchProducts(){
+  FetchProducts() {
     this.service.getProducts().subscribe(
-      res => {
-        this.productList = res
+      (res) => {
+        this.productList = res;
         console.log('Products fetched');
         console.log(this.productList);
       },
-      err => {
-        this.productList = []
+      (err) => {
+        this.productList = [];
         console.log('error occured');
       }
-    )
+    );
   }
 
-
-  Buy(p:IProduct){
-    this.router.navigate(['/payment', p.productID,p.productPrice,p.productName]);
+  Buy(p: IProduct) {
+    this.router.navigate([
+      '/payment',
+      p.productID,
+      p.productPrice,
+      p.productName,
+    ]);
   }
   //this method will run when the add membership will be clicked
-
-
 }
 
-type LoggedInUser='' | null |string;
-
+type LoggedInUser = '' | null | string;
